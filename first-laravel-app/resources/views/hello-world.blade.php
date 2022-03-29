d7ced912da8a9d5280c8c8784e7de7907692ce74
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-29 17:09:40 +0200

    Add a form

--- first-laravel-app/resources/views/hello-world.blade.php
+++ first-laravel-app/resources/views/hello-world.blade.php
@@ -4,4 +4,13 @@
     <h2>Hello World</h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, nemo magni. Soluta voluptatum hic illum rerum
         voluptatem, debitis iusto ratione inventore, corporis, voluptate molestiae vero. Ipsa dolorem ut iure nihil!</p>
+    {{ $errors }}
+    <form action="" method="POST">
+        @csrf
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder="example@email.com">
+        <label for="password">password: </label>
+        <input type="password" name="password" id="password" placeholder="Enter password here...">
+        <input type="submit" value="CLICK ME">
+    </form>
 @endsection
