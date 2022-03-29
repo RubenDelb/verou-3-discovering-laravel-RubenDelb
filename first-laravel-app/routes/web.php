b8b6ea8b81832815cf646e298d23facb857191d8
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-29 09:32:03 +0200

    Create named routes

--- first-laravel-app/routes/web.php
+++ first-laravel-app/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
 
-Route::get('/first-test', function () {
-    return view('first-test');
-});
+Route::get('/hello-world', [PageController::class, 'helloWorld'])->name('hello-world');
+
+Route::get('/goodbye-world', [PageController::class, 'goodbyeWorld'])->name('goodbye-world');
