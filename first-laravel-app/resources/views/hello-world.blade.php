cd8e7787d88fe7a17180bf923ebf8c6f0edcc89d
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-29 11:48:13 +0200

    Implement the templates on the page.

--- first-laravel-app/resources/views/hello-world.blade.php
+++ first-laravel-app/resources/views/hello-world.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <h1>Hello World</h1>
-        <a href="{{route('goodbye-world')}}">Goodbye-World page</a><br>
-    <a href="/">Homepage</a>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('body')
+
+<h2>Hello World</h2>
+<a href="{{route('goodbye-world')}}">Goodbye-World page</a><br>
+<a href="/">Homepage</a>
+
+@endsection
--- first-laravel-app/resources/views/welcome.blade.php
+++ first-laravel-app/resources/views/welcome.blade.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First Laravel App</title>
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="/app.css">
+@section('body')
 
-</head>
-<body>
-    <h1>Welcome on my first Laravel-site!</h1>
-    <a href="{{route('hello-world')}}">Hello-World page</a><br>
-    <a href="{{route('goodbye-world')}}">Goodbye-World page</a>
+<h2>Welcome on my first Laravel-site!</h2>
+<a href="{{route('hello-world')}}">Hello-World page</a><br>
+<a href="{{route('goodbye-world')}}">Goodbye-World page</a>
 
-    
-</body>
-</html>
+@endsection
