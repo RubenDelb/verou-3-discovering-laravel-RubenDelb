d180d1ab57339046ba75c55f5d007b598dc7a5a8
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-30 16:59:44 +0200

    Add functions to create and update recipes in database

--- first-laravel-app/app/Http/Controllers/RecipeController.php
+++ first-laravel-app/app/Http/Controllers/RecipeController.php
@@ -9,8 +9,43 @@ class RecipeController extends Controller
 {
     public function getAllRecipes()
     {
-
         $recipes = Recipe::get(); // Create a collection out of my database and store it in the variable recipes
         return view('recipes', compact('recipes'));
+    }
+
+    public function createNewRecipe(Request $newRecipeData)
+    {
+        $recipeData = array('title' => $newRecipeData->recipeTitle,
+            'description' => $newRecipeData->recipeDescription,
+            'origin' => $newRecipeData->recipeOrigin,
+            'difficulty' => $newRecipeData->recipeDifficulty,
+            'duration' => $newRecipeData->recipeDuration,
+            'image_link' => $newRecipeData->recipeImageLink,
+            'link_to_recipe' => $newRecipeData->recipeLink,
+        );
+        Recipe::create($recipeData);
+        return $this->getAllRecipes();
+    }
+
+    public function showUpdateForm(Request $req)
+    {
+        // dd($req->id);
+        $recipe = Recipe::where('id', $req->id)->first();
+        // dd($recipe);
+        return view('update-recipe', compact('recipe'));
+    }
+
+    public function updateRecipe(Request $newRecipeData)
+    {
+        $recipeData = array('title' => $newRecipeData->recipeTitle,
+            'description' => $newRecipeData->recipeDescription,
+            'origin' => $newRecipeData->recipeOrigin,
+            'difficulty' => $newRecipeData->recipeDifficulty,
+            'duration' => $newRecipeData->recipeDuration,
+            'image_link' => $newRecipeData->recipeImageLink,
+            'link_to_recipe' => $newRecipeData->recipeLink,
+        );
+        Recipe::where('id', $newRecipeData->id)->update($recipeData);
+        return $this->getAllRecipes();
     }
 }
