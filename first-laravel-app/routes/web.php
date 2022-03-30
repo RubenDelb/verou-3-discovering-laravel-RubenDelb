1952842b309e4bdb1666c99eda2840b182e88f1e
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-03-30 17:01:57 +0200

    Add new routes

--- first-laravel-app/routes/web.php
+++ first-laravel-app/routes/web.php
@@ -19,9 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\PageController::class, 'index']);
 
 Route::get('/hello-world', [App\Http\Controllers\PageController::class, 'helloWorld'])->name('hello-world');
-
 Route::post('/hello-world', [App\Http\Controllers\PageController::class, 'emailSubmitted']);
 
 Route::get('/goodbye-world', [App\Http\Controllers\PageController::class, 'goodbyeWorld'])->name('goodbye-world');
 
 Route::get('/recipes', [RecipeController::class, 'getAllRecipes'])->name('recipes');
+Route::post('/recipes', [RecipeController::class, 'createNewRecipe']);
+
+
+Route::get('/update-recipe', [RecipeController::class, 'showUpdateForm'])->name('update-recipe');
+
+Route::post('/update-recipe', [RecipeController::class, 'updateRecipe']);
